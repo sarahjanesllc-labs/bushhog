68897e41b09a372c211cb75b49a4ea2cb3e42421
Adam Stokes <adam.stokes@ubuntu.com>
2014-06-29 00:42:31 -0400

    add bootstrap
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,49 @@
 var gulp = require('gulp'),
     nib = require('nib'),
-    stylus = require('gulp-stylus'),
+    less = require('gulp-less'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     nodemon = require('gulp-nodemon');
 
 gulp.task('styles', function() {
-    gulp.src('stylus/index.styl')
-    .pipe(stylus({use: [nib()]}))
-    .pipe(gulp.dest('static/css/'));
+    gulp.src('less/index.less')
+        .pipe(less({compress: true}))
+        .pipe(gulp.dest('static/css/'));
+});
+
+var vendorCSS = [
+    "bower_components/bootswatch/cosmo/bootstrap.min.css",
+];
+
+var vendorScripts = [
+    "bower_components/jquery/**/jquery.min.js",
+    "bower_components/bootstrap/**/**/bootstrap.min.js"
+];
+
+gulp.task('concatCSS', function() {
+    gulp.src(vendorCSS)
+        .pipe(concat('vendor.min.css'))
+        .pipe(gulp.dest('static/css/'));
+});
+
+gulp.task('concatJS', function() {
+    gulp.src(vendorScripts)
+        .pipe(uglify())
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('static/js/'));
 });
 
 gulp.task('develop', function() {
     process.env.NODE_ENV = 'dev';
-    nodemon({ script: 'server.js', ext: 'hbs styl js', ignore: ['node_modules/', 'test/']})
-    .on('change', ['styles'])
-    .on('restart', function() {
-        console.log('restarted');
-    });
+    nodemon({
+        script: 'server.js',
+        ext: 'hbs less js',
+        ignore: ['node_modules/', 'test/']
+    })
+        .on('change', ['styles'])
+        .on('restart', function() {
+            console.log('restarted');
+        });
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['styles', 'concatCSS', 'concatJS']);
--- server.js
+++ server.js
@@ -6,6 +6,24 @@ Mongoose.connect('mongodb://localhost/rfmproducetogo');
 
 var server = new Hapi.Server(8080, "localhost", config.server);
 
+server.route({
+    path: '/favicon.ico',
+    method: 'GET',
+    handler: { file: './favicon.ico' }
+});
+
+server.route({
+    path: '/static/{path*}',
+    method: 'GET',
+    handler: {
+        directory: {
+            path: './static',
+            listing: false,
+            index: false
+        }
+    }
+});
+
 server.pack.register([{
     plugin: require("lout")
 }, {
